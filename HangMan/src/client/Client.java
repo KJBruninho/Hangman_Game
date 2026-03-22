919d0909aafa7df5209b66157aabc71231248955
Bruno Marinho <120292056+KJBruninho@users.noreply.github.com>
2026-03-22 03:39:41 +0000

    Add files via upload

--- HangMan/src/client/Client.java
+++ HangMan/src/client/Client.java
@@ -2,29 +2,24 @@ package client;
 
 import java.io.*;
 import java.net.*;
+
 import utils.Message;
 
 public class Client {
 
 	private Socket s;
 	
-	
-	//Remover mais tarde a main... 
-	//Apenas teste.
-	public static void main(String[] args ) throws ClassNotFoundException, IOException {
-		Client teste = new Client();
-		teste.s.close();
-	}
-	
 	public Client(){
 		try {
 			s = new Socket("127.0.0.1", 5432);
-			//Remover mais tarde.Colocar a restante logica.
 			Message.receiveMessage(s);
-			s.close();
 		} catch (IOException | ClassNotFoundException e) {
 	        e.printStackTrace();
 	    }
+	}
+	
+	public Socket getSocket() {
+		return s;
 	}
 	
 }
--- HangMan/src/game/Game.java
+++ HangMan/src/game/Game.java
@@ -1,8 +1,21 @@
 package game;
 
+import java.io.IOException;
+import java.net.Socket;
+
+import client.Client;
+
 public class Game {
 
 	public static void main(String[] args) {
+		
+		Client teste = new Client();
+		Socket ClientSocket = teste.getSocket();
+		try {
+			ClientSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 }
